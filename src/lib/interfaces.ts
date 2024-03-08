47d905639fb3daaaf0536efc231015e7d2f545ec
Dogeek <simon.bordeyne@gmail.com>
2024-03-09 00:21:09 +0100

    add mods page

--- src/lib/interfaces.ts
+++ src/lib/interfaces.ts
@@ -4,3 +4,22 @@ export interface HomePageData {
   windowsDownloadUrl: string;
   macDownloadUrl: string;
 }
+
+export interface Mod {
+  slug: string;
+  version: string;
+  name: string;
+  description: string;
+  url: string;
+}
+
+export interface ModRepository {
+  mods: Mod[];
+  repoUrl: string;
+  owner: string;
+  repo: string;
+}
+
+export interface ModsPageData {
+  repos: ModRepository[];
+}

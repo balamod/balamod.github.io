79b8a810d7043430041f410df18bbd25b074f5c3
Dogeek <simon.bordeyne@gmail.com>
2024-03-08 22:15:17 +0100

    Github page with Sveltekit

---
+++ src/lib/interfaces.ts
@@ -0,0 +1,6 @@
+export interface HomePageData {
+  releaseVersion: string;
+  linuxDownloadUrl: string;
+  windowsDownloadUrl: string;
+  macDownloadUrl: string;
+}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import TopAppBar, { Section, Row } from '@smui/top-app-bar';
+  import Button, { Label } from '@smui/button';
+  import { faGithub } from '@fortawesome/free-brands-svg-icons';
+  import Fa from 'svelte-fa';
+</script>
+
+<body>
+  <div class="flexy">
+    <div class="top-app-bar-container flexor">
+      <TopAppBar variant="static" dense={true} color="primary">
+        <Row>
+          <Section>
+            <Button href="/"><Label>Home</Label></Button>
+            <Button href="/docs"><Label>Documentation</Label></Button>
+            <Button href="/mods"><Label>Mods</Label></Button>
+          </Section>
+          <Section align="end">
+            <Button href="https://github.com/UwUDev/balamod" target="_blank">
+              <Fa icon={faGithub} scale={1.5}/>
+            </Button>
+          </Section>
+        </Row>
+      </TopAppBar>
+      <div class="flexor-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</body>
+
+<style>
+  .top-app-bar-container {
+    width: 100vw;
+    height: calc(100vh - 20px);
+    border: 1px solid var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+    margin: 0 18px 18px 0;
+    background-color: var(--mdc-theme-background, #fff);
+
+    overflow: auto;
+    display: inline-block;
+  }
+
+  @media (max-width: 480px) {
+    .top-app-bar-container {
+      margin-right: 0;
+    }
+  }
+
+  .flexy {
+    display: flex;
+    flex-wrap: wrap;
+    overflow-y: hidden;
+  }
+
+  .flexor {
+    display: inline-flex;
+    flex-direction: column;
+  }
+
+  .flexor-content {
+    flex-basis: 0;
+    height: 0;
+    flex-grow: 1;
+    overflow: auto;
+  }
+</style>
